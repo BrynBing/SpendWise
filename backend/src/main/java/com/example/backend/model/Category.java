0ac326f95f1ccfb2c21e6da4d8f0c02a916c5e27
scui0452 <scui0452@uni.syndy.edu.au>
2025-09-19 19:08:22 +1000

    fix: modify Category entity to avoid name error

--- backend/src/main/java/com/example/backend/model/Category.java
+++ backend/src/main/java/com/example/backend/model/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -15,22 +16,22 @@ import java.time.LocalDateTime;
         @UniqueConstraint(columnNames = "category_name")
 })
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
-    private Integer category_id;
+    private Integer categoryId;
 
     @Column(name = "category_name", nullable = false, unique = true)
-    private String category_name;
+    private String categoryName;
 
     @Column(name = "description")
     private String description;
 
     @Column(name = "icon_url")
-    private String icon_url;
+    private String iconUrl;
 
-    @Column(name = "created_at", updatable = false, insertable = false,
+    @Column(name = "created_at", updatable = false,
             columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private LocalDateTime created_at;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
